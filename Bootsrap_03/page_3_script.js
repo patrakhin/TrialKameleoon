fce27a73c4dbe82941b87dd12167ece50eb91e9d
patrakhin <patrakhin@mail.ru>
2023-11-11 00:24:40 +0300

    added fix page_3 and script

--- Bootsrap_03/page_3_script.js
+++ Bootsrap_03/page_3_script.js
@@ -1,35 +1,42 @@
-// Выполняем GET-запрос на сервер для получения данных
-fetch('http://localhost:8080/pagination_manager1/vehicles?page=0&size=10', {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token') // Прикрепляем токен из localStorage
-    }
-})
-.then(response => {
-    // Проверяем успешность ответа
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    // Парсим JSON-ответ
-    return response.json();
-})
-.then(dataFromServer => {
-    // Обрабатываем полученные данные здесь
-    const tableBody = document.getElementById('vehicleTableBody');
-    const paginationContainer = document.getElementById('paginationButtons');
-    const currentPageSpan = document.getElementById('currentPage');
-    const totalPagesSpan = document.getElementById('totalPages');
-    
+document.addEventListener('DOMContentLoaded', function () {
+    // Начальные данные для первой загрузки страницы
     const itemsPerPage = 10;
     let currentPage = 0;
+    let dataFromServer; // Добавим переменную для хранения данных
 
-    function displayDataOnPage(data, page) {
+    // Найти контейнер для пагинации
+    const paginationContainer = document.getElementById('paginationButtons');
+
+    // Функция для выполнения GET-запроса к бэкенду
+    function fetchData(page) {
+        fetch(`http://localhost:8080/pagination_manager1/vehicles?page=${page}&size=${itemsPerPage}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token') // Прикрепляем токен из localStorage
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            dataFromServer = data; // Сохраняем данные
+            displayDataOnPage(data.content);
+            updatePaginationButtons(data.totalPages, page);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    }
+
+    // Функция для отображения данных на странице
+    function displayDataOnPage(data) {
+        const tableBody = document.getElementById('vehicleTableBody');
         tableBody.innerHTML = ''; // Очищаем содержимое таблицы
-        const startIndex = page * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const pageItems = data.slice(startIndex, endIndex);
-
-        pageItems.forEach(item => {
+    
+        data.forEach(item => {
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td>${item.id}</td>
@@ -43,32 +50,44 @@ fetch('http://localhost:8080/pagination_manager1/vehicles?page=0&size=10', {
             `;
             tableBody.appendChild(row);
         });
-
+    
         // Устанавливаем текущую страницу и общее количество страниц
-        currentPageSpan.textContent = currentPage + 1;
-        totalPagesSpan.textContent = Math.ceil(data.length / itemsPerPage);
+        document.getElementById('currentPage').textContent = currentPage + 1;
+        document.getElementById('totalPages').textContent = dataFromServer.totalPages;
     }
 
+    // Функция для обновления кнопок пагинации
     function updatePaginationButtons(totalPages, currentPage) {
         paginationContainer.innerHTML = ''; // Очищаем кнопки пагинации
-        for (let i = 0; i < totalPages; i++) {
-            const li = document.createElement('li');
-            li.className = `page-item ${i === currentPage ? 'active' : ''}`;
-            li.innerHTML = `<a class="page-link" href="#" data-page="${i}">${i + 1}</a>`;
-            li.addEventListener('click', function (event) {
-                event.preventDefault();
-                currentPage = parseInt(event.target.getAttribute('data-page'));
-                displayDataOnPage(dataFromServer.content, currentPage);
-                updatePaginationButtons(totalPages, currentPage);
-            });
-            paginationContainer.appendChild(li);
-        }
+    
+        const liPrevious = document.createElement('li');
+        liPrevious.className = 'page-item';
+        liPrevious.innerHTML = `<a class="page-link" href="#" data-page="${currentPage - 1}">Предыдущая</a>`;
+        liPrevious.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (currentPage > 0) {
+                fetchData(currentPage - 1);
+            }
+        });
+        paginationContainer.appendChild(liPrevious);
+    
+        const liNext = document.createElement('li');
+        liNext.className = 'page-item';
+        liNext.innerHTML = `<a class="page-link" href="#" data-page="${currentPage + 1}">Следующая</a>`;
+        liNext.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (currentPage < totalPages - 1) {
+                fetchData(currentPage + 1);
+            }
+        });
+        paginationContainer.appendChild(liNext);
+    
+        const liCounter = document.createElement('li');
+        liCounter.className = 'page-item disabled';
+        liCounter.innerHTML = `<span class="page-link">Страница ${currentPage + 1} из ${totalPages}</span>`;
+        paginationContainer.appendChild(liCounter);
     }
 
-    // Показываем данные на первой странице при загрузке
-    displayDataOnPage(dataFromServer.content, currentPage);
-    updatePaginationButtons(Math.ceil(dataFromServer.totalElements / itemsPerPage), currentPage);
-})
-.catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
+    // Загрузка данных для первой страницы при загрузке страницы
+    fetchData(currentPage);
 });
